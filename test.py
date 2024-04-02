20ae9922a0dcaac0c8de9718e5bc57dc2db0140e
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-04-02 13:56:45 +0900

    variable python

--- test.py
+++ test.py
@@ -4,8 +4,12 @@
 
 # print("programming")
 
-print(1+1)
-print(1-1)
-print(5*1)
-print(10/2)
-print(5%3)
+# print(1+1)
+# print(1-1)
+# print(5*1)
+# print(10/2)
+# print(5%3)
+
+andrew = 'hello world'
+login_id = 'adfdkasg24442'
+print(login_id)
