e83d5872fae64b71abab4dee46bd3e88f2ccbeb0
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-04-02 14:20:16 +0900

    function

--- test.py
+++ test.py
@@ -14,13 +14,23 @@
 # login_id = 'adfdkasg24442'
 # print(login_id)
 
-a = 'Hello world'
-a_length = 5
-a_times = 5.5
+# a = 'Hello world'
+# a_length = 5
+# a_times = 5.5
 
-if a_length > 6:
-    print('tate')
-elif a_length == 5:
-    print('private jet')
-else:
-    print('tristan')
+# if a_length > 6:
+#     print('tate')
+# elif a_length == 5:
+#     print('private jet')
+# else:
+#     print('tristan')
+
+def andrew(arg):
+    andrew_status = arg
+    
+    if(andrew_status < 10):
+        return 'stay hard'
+    else:
+        return 'billionaire'
+
+print(andrew(5))
