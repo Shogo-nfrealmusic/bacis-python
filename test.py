598aba85c1343690d5101d2acbbaf4e93696bc14
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-04-02 15:35:17 +0900

    finish basic python

--- test.py
+++ test.py
@@ -47,20 +47,38 @@
 # with open('./andrew.txt', 'r') as file:
 #     print(file.read())
 
-class Card:
-    def __init__(self, date, user_name):
-        self.date = date
-        self.user_name = user_name
-    def message(self):
-        return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました。'
+# class Card:
+#     def __init__(self, date, user_name):
+#         self.date = date
+#         self.user_name = user_name
+#     def message(self):
+#         return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました。'
         
-date_a = '2020-01-01'
-user_name_a = 'taro'
+# date_a = '2020-01-01'
+# user_name_a = 'taro'
 
-date_b = '2023-01-01'
-user_name_b = 'matsuo'
+# date_b = '2023-01-01'
+# user_name_b = 'matsuo'
 
-card_b = Card(date_b, user_name_b)
+# card_b = Card(date_b, user_name_b)
 
-print(card_b.message())
+# print(card_b.message())
+
+# import math
+# print(math.pi)
+
+#NumPy
+#Pandas
+#Flask
+#Django
+
+import numpy
+
+numpy_list = [3, 1, 5, 10, 2093, 2, 4, 2039]
+print(numpy.sum(numpy_list))
+
+# import sys
+# import pprint
+
+# pprint.pprint(sys.path)
 
