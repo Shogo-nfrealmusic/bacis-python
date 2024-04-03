94200882e54dbc0ef2f5b99fdde411ec97604aa9
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-04-03 21:15:19 +0900

    other source

--- test.py
+++ test.py
@@ -1,84 +1,84 @@
-# print("Hello World!")
+print("Hello World!")
 
-# print("アイういお")
+print("アイういお")
 
-# print("programming")
+print("programming")
 
-# print(1+1)
-# print(1-1)
-# print(5*1)
-# print(10/2)
-# print(5%3)
+print(1+1)
+print(1-1)
+print(5*1)
+print(10/2)
+print(5%3)
 
-# andrew = 'hello world'
-# login_id = 'adfdkasg24442'
-# print(login_id)
+andrew = 'hello world'
+login_id = 'adfdkasg24442'
+print(login_id)
 
-# a = 'Hello world'
-# a_length = 5
-# a_times = 5.5
+a = 'Hello world'
+a_length = 5
+a_times = 5.5
 
-# if a_length > 6:
-#     print('tate')
-# elif a_length == 5:
-#     print('private jet')
-# else:
-#     print('tristan')
+if a_length > 6:
+    print('tate')
+elif a_length == 5:
+    print('private jet')
+else:
+    print('tristan')
 
-# def andrew(arg):
-#     andrew_status = arg
+def andrew(arg):
+    andrew_status = arg
     
-#     if(andrew_status < 10):
-#         return 'stay hard'
-#     else:
-#         return 'billionaire'
+    if(andrew_status < 10):
+        return 'stay hard'
+    else:
+        return 'billionaire'
 
-# print(andrew(5))
+print(andrew(5))
 
-# billionaire_list = ['andrew_tate', 'tristan_tate', 'elon_musk']
-# print(billionaire_list[0])
+billionaire_list = ['andrew_tate', 'tristan_tate', 'elon_musk']
+print(billionaire_list[0])
 
-# for index in range(15):
-#     print(andrew(index))
+for index in range(15):
+    print(andrew(index))
 
-# for item in billionaire_list:
-    # print(item)
+for item in billionaire_list:
+    print(item)
     
-# with open('./andrew.txt', 'r') as file:
-#     print(file.read())
+with open('./andrew.txt', 'r') as file:
+    print(file.read())
 
-# class Card:
-#     def __init__(self, date, user_name):
-#         self.date = date
-#         self.user_name = user_name
-#     def message(self):
-#         return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました。'
+class Card:
+    def __init__(self, date, user_name):
+        self.date = date
+        self.user_name = user_name
+    def message(self):
+        return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました。'
         
-# date_a = '2020-01-01'
-# user_name_a = 'taro'
+date_a = '2020-01-01'
+user_name_a = 'taro'
 
-# date_b = '2023-01-01'
-# user_name_b = 'matsuo'
+date_b = '2023-01-01'
+user_name_b = 'matsuo'
 
-# card_b = Card(date_b, user_name_b)
+card_b = Card(date_b, user_name_b)
 
-# print(card_b.message())
+print(card_b.message())
 
-# import math
-# print(math.pi)
+import math
+print(math.pi)
 
-#NumPy
-#Pandas
-#Flask
-#Django
+NumPy
+Pandas
+Flask
+Django
 
 import numpy
 
 numpy_list = [3, 1, 5, 10, 2093, 2, 4, 2039]
 print(numpy.sum(numpy_list))
 
-# import sys
-# import pprint
+import sys
+import pprint
 
-# pprint.pprint(sys.path)
+pprint.pprint(sys.path)
 
